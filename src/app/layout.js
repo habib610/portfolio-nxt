b8fd0696bdf33bd540a917abd4789945c38f654d
habib610 <habib54562@gmail.com>
2025-10-04 16:32:21 +0600

    added-meta-data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { OG_IMAGES } from "@/constants/routes";
 import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
@@ -19,8 +20,47 @@ const nunitoSans = Nunito_Sans({
 });
 
 export const metadata = {
-    title: "HABIB | PORTFOLIO",
-    description: "Welcome to personal portfolio of Habib",
+    title: "Habib | Full Stack Developer",
+    description:
+        "Full-Stack Software Engineer with 2+ years of experience building scalable web applications using React, Next.js, Java, and Spring Boot.",
+    keywords: [
+        "Habibur Rahman",
+        "Habib",
+        "Full Stack Developer",
+        "Full Stack Engineer",
+        "React Developer",
+        "Next.js Developer",
+        "Java Spring Boot",
+        "Software Engineer Portfolio",
+        "Developer Portfolio",
+        "Nextjs Developer Portfolio",
+    ],
+    authors: [{ name: "Habibur Rahman" }],
+    openGraph: {
+        title: "Habibur Rahman | Full Stack Developer",
+        description:
+            "Building modern, accessible, and performant web applications with React, Next.js, and Java Spring Boot.",
+        url: "https://rhabib.dev",
+        siteName: "Habibur Rahman Portfolio",
+        images: [
+            {
+                url: OG_IMAGES,
+                width: 1200,
+                height: 630,
+                alt: "Habibur Rahman Portfolio Preview",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Habibur Rahman | Full Stack Developer",
+        description:
+            "Building modern, accessible, and performant web applications with React, Next.js, and Java Spring Boot.",
+        images: [OG_IMAGES],
+        creator: "@habib_610",
+    },
 };
 
 export default function RootLayout({ children }) {
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,3 +4,6 @@ export const NAV_LINKS = [
     { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
 ];
+
+export let OG_IMAGES =
+    "https://firebasestorage.googleapis.com/v0/b/p-cms610.appspot.com/o/og-images%2Fog-image-blue_v2.jpg?alt=media&token=8dd5aeb3-694b-46bb-9034-8fd68d91b858";
